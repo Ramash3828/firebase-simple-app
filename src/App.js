cc2c71a70afffc26e4312182a46445271d4bb3d0
Ramash3828 <ramash3828@gmail.com>
2022-04-07 15:47:06 +0600

    firebase-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
+import app from "./Firebase.init";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+
+const auth = getAuth(app);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [validated, setValidated] = useState(false);
+
+    const [registered, setRegistered] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailField = (e) => {
+        setEmail(e.target.value);
+    };
+    const handlePassField = (e) => {
+        setPassword(e.target.value);
+    };
+    const handleRegChange = (e) => {
+        setRegistered(e.target.checked);
+    };
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+            return;
+        }
+
+        setValidated(true);
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((res) => {
+                const user = res.user;
+                console.log(user);
+                setEmail("");
+                setPassword("");
+            })
+            .catch((err) => {
+                console.log("Error", err);
+            });
+        e.preventDefault();
+    };
+    return (
+        <div className="w-50 mx-auto mt-3">
+            <div className="container">
+                <h2 className="text-primary mb-3">
+                    Please {registered ? "Login" : "Register"}!!
+                </h2>
+                <Form
+                    noValidate
+                    validated={validated}
+                    onSubmit={handleFormSubmit}
+                >
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            onBlur={handleEmailField}
+                            type="email"
+                            placeholder="Enter email"
+                            required
+                        />
+                        {/* <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text> */}
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Email.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+
+                        <Form.Control
+                            onBlur={handlePassField}
+                            type="password"
+                            placeholder="Password"
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Password.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check
+                            onChange={handleRegChange}
+                            type="checkbox"
+                            label="Already Registered"
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        {registered ? "Login" : "Register"}
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBSvJaudsQKMdpKtvtll8VEVVle9cuMkTE",
+    authDomain: "fir-simple-app-a302c.firebaseapp.com",
+    projectId: "fir-simple-app-a302c",
+    storageBucket: "fir-simple-app-a302c.appspot.com",
+    messagingSenderId: "1096967278304",
+    appId: "1:1096967278304:web:38018f3b02616d2ce7ca85",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
